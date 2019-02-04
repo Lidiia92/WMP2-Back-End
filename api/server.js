4e5a84d7793f6d9cf6c97b65ce4a400bb1bae0a9
Lidii <lidiiany92@gmail.com>
2019-02-04 14:48:26 -0500

    Amendments

--- api/server.js
+++ api/server.js
@@ -20,6 +20,6 @@ server.use("/api/register/", registerRoute);
 server.use("/api/users/", userRoutes);
 server.use("/api/plants/", plantsRoute);
 server.use("/api/usersunp/", usersUnpRoutes);
-erver.use("/api/plantsunp/", plantsUnpRoutes);
+server.use("/api/plantsunp/", plantsUnpRoutes);
 
 module.exports = server;
